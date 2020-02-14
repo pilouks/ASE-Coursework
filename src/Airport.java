4abdd8e16696d6b0993f67a96be5a19804723f92
Ozzyben <ben@rathobyres.co.uk>
2020-02-14 13:48:21 +0000

    Comments

--- src/Airport.java
+++ src/Airport.java
@@ -10,228 +10,233 @@ public class Airport {
     private HashMap<String, Flight> planes;
     private CheckInGUI GUI;
 
-    private int maxBagSize = 5;
-    private int maxBagWeight = 100;
-    private int feePerExtraBag = 25;
-    private int excessBagSize = 3;
-    private int excessBagWeight = 20;
-    private int excessFee = 50;
+    private int maxBagSize = 5;         //max size a bag can be
+    private int maxBagWeight = 100;     //max weight a bag can be
+    private int feePerExtraBag = 25;    //fee for each bag after first
+    private int excessBagSize = 3;      //size bag can be before excess fees apply
+    private int excessBagWeight = 20;   //weight a bag can be before excess fees apply
+    private int excessFee = 50;         //the excess fee needed for over volume/weight
 
-    public Airport(){
+    public Airport() {
         waitingRoom = new HashMap<>();
         planes = new HashMap<>();
         GUI = new CheckInGUI();
     }
 
+    /*Main, duh*/
+    public static void main(String[] args) {
+        Airport a = new Airport();      //object
+        a.run();                        //run it
+    }
+
     /*Will be called main
-    * Contains code for main running loop/process
-    * Non static content*/
-    public void run(){
-        //to do
+     * Contains code for main running loop/process
+     * Non static content*/
+    public void run() {
+        //done
         Boolean running = true;
         readFlightData("FlightData");
         readPassengerData("PassengerData");
 
-        GUI.createAndShowGUI(); //Generate gui
+        GUI.createAndShowGUI();                                                 //Generate gui
         do {
-            String userInput = GUI.getUserInput(); //Request first contact
-            if (userInput.toLowerCase().trim().equals("exit")) { //exit option chosen
-                writeReport(); //gen reports
-                running = false; //end loop
-            } else { //exit not chosen so booking reference and last name requested
-                String[] userInfo = userInput.split(","); //separate into ref and name
+            String userInput = GUI.getUserInput();                              //Request first contact
+            if (userInput.toLowerCase().trim().equals("exit")) {                //exit option chosen
+                System.out.println(writeReport());                              //gen reports
+                running = false;                                                //end loop
+            } else {                                                            //exit not chosen so booking reference and last name requested
+                String[] userInfo = userInput.split(",");                //separate into ref and name
                 String ref = userInfo[0].trim();
                 String name = userInfo[1].trim();
-                if (waitingRoom.containsKey(ref)) { //does given reference key apply to person in waiting room
-                    Passenger current = waitingRoom.get(ref); //Yes so pull persons data
-                    if (current.getLastName().equals(name)) { //does the last name given match the ref
-                        double passengerFees = 0.0; //Fees for baggage
-                        int bagNum = 0; //Number of bags added
-                        double tempFee = addBagToPassenger(ref, bagNum); //Attempt to add first bag to passenger
-                        while(tempFee >= 0.0){ //While user wants to add a bag
-                            passengerFees += tempFee; //add new fee to total
+                if (waitingRoom.containsKey(ref)) {                             //does given reference key apply to person in waiting room
+                    if (waitingRoom.get(ref).getLastName().equals(name)) {      //does the last name given match the ref
+                        double passengerFees = 0.0;                             //Fees for baggage
+                        int bagNum = 0;                                         //Number of bags added
+                        double tempFee = addBagToPassenger(ref, bagNum);        //Attempt to add first bag to passenger
+                        while (tempFee >= 0.0) {                                //While user wants to add a bag
+                            passengerFees += tempFee;                           //add new fee to total
                             bagNum++;
-                            tempFee = addBagToPassenger(ref,bagNum); //attempt to add another bag to passenger
+                            tempFee = addBagToPassenger(ref, bagNum);           //attempt to add another bag to passenger
                         }
-                        planes.get(current.getFlightCode()).checkInToFlight(current, passengerFees); //find plane customer is on and check customer onto it
-                        waitingRoom.remove(ref); //remove customer from waiting room
+                        planes.get(waitingRoom.get(ref).getFlightCode()).checkInToFlight(waitingRoom.get(ref), passengerFees); //find plane customer is on and check customer onto it
+                        waitingRoom.remove(ref);                                //remove customer from waiting room
                     } else {
-                        GUI.ErrorScreen(2); //last name and ref dont match show error screen
+                        GUI.ErrorScreen(2);                          //last name and ref dont match show error screen
                     }
                 } else {
-                    GUI.ErrorScreen(1); //reference doesnt exist show error screen
+                    GUI.ErrorScreen(1);                              //reference doesnt exist show error screen
                 }
             }
-        }while(running); //keep running
+        } while (running);                                                      //keep running
     }
 
     /*Will read in flight data from file
-    * And store within flight map*/
-    private void readFlightData(String fileName){
+     * And store within flight map*/
+    private void readFlightData(String fileName) {
         //done
-        BufferedReader br = null;
+        BufferedReader br = null; //reader
 
-        try{
-            br = new BufferedReader(new FileReader(fileName));
-            String inputLine = br.readLine();
-            while(inputLine != null){
-                String[] data = inputLine.split(",");
-                for(int i = 0; i < data.length; i++){
-                    data[i] = data[i].trim();
+        try {
+            br = new BufferedReader(new FileReader(fileName));      //file reader
+            String inputLine = br.readLine();                       //read the first line
+            while (inputLine != null) {                             //while its got stuff
+                String[] data = inputLine.split(",");        //split by commas
+                for (int i = 0; i < data.length; i++) {
+                    data[i] = data[i].trim();                       //trim off white space
                 }
-                Flight f = new Flight(data[0], data[1], data[2], Integer.parseInt(data[3]), Integer.parseInt(data[4]), Integer.parseInt(data[5]));
-                planes.put(data[0], f);
-                inputLine = br.readLine();
+                Flight f = new Flight(data[0], data[1], data[2], Integer.parseInt(data[3]), Integer.parseInt(data[4]), Integer.parseInt(data[5])); //make new flight object using read data
+                planes.put(data[0], f);                             //put the flight in the map with flight code as ref
+                inputLine = br.readLine();                          //read the next line
             }
-
-        } catch(FileNotFoundException e) {
+        } catch (FileNotFoundException e) {
             System.out.println("FLight data file not found\n" + e);
             System.exit(0);
-        } catch(IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
             System.exit(0);
         } finally {
-            try{
+            try {
                 br.close();
-            } catch(IOException e){
+            } catch (IOException e) {
                 //do nothing
             }
         }
     }
 
     /*Will read in passenger data from file
-    * and store within passenger map waitingRoom*/
-    private void readPassengerData(String fileName){
+     * and store within passenger map waitingRoom*/
+    private void readPassengerData(String fileName) {
         //done
-        BufferedReader br = null;
+        BufferedReader br = null; //reader
 
-        try{
-            br = new BufferedReader(new FileReader(fileName));
-            String inputLine = br.readLine();
-            while(inputLine != null){
-                String[] data = inputLine.split(",");
-                for(String item : data){
-                    item.trim();
+        try {
+            br = new BufferedReader(new FileReader(fileName));      //file reader
+            String inputLine = br.readLine();                       //read first line
+            while (inputLine != null) {                             //while its got data on it
+                String[] data = inputLine.split(",");        //split up by commas
+                for (int i = 0; i < data.length; i++) {
+                    data[i] = data[i].trim();                       //trim off white space
                 }
-                Boolean checked = Boolean.parseBoolean(data[4]);
-                Passenger p = new Passenger(data[0], data[1], data[2], data[3], checked);
-                if(checked){
-                    planes.get(data[3]).addPassenger(p);
-                } else {
-                    waitingRoom.put(data[0], p);
+                Boolean checked = Boolean.parseBoolean(data[4]);    //make checked in into boolean
+                Passenger p = new Passenger(data[0], data[1], data[2], data[3], checked); //create new passenger from line info
+                if (checked) {                                      //if they've already checked in
+                    planes.get(data[3]).addPassenger(p);            //add directly to the flight
+                } else {                                            //if not
+                    waitingRoom.put(data[0], p);                    //add to waiting room
                 }
-                inputLine = br.readLine();
+                inputLine = br.readLine();                          //read next line
             }
-        } catch(FileNotFoundException e) {
-            System.out.println("FLight data file not found\n" + e);
+        } catch (FileNotFoundException e) {
+            System.out.println("Passenger data file not found\n" + e);
             System.exit(0);
-        } catch(IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
             System.exit(0);
         } finally {
-            try{
+            try {
                 br.close();
-            } catch(IOException e){
+            } catch (IOException e) {
                 //do nothing
             }
         }
+        //literally a copy paste from flight reader
     }
 
     /*Will call gui to get bag information
-    * Will verify baggage size and weight
-    * Will call gui to request excess fees if required
-    * Will add bag to passenger and return with excess fees paid*/
-    private double addBagToPassenger(String reference, int bagNumber){
+     * Will verify baggage size and weight
+     * Will call gui to request excess fees if required
+     * Will add bag to passenger and return with excess fees paid*/
+    private double addBagToPassenger(String reference, int bagNumber) {
         //done
-        List<Double> info = GUI.getPassengerBagInfo();
-        if(info.get(0) < 0) {
-            return -1.0;
-        } else {
-            double bagVol = info.get(0);
-            double bagWeight = info.get(1);
-            while ((bagVol > maxBagSize) || (bagWeight > maxBagWeight)) {
-                GUI.ErrorScreen(3);
-                info = GUI.getPassengerBagInfo();
-                bagVol = info.get(0);
-                bagWeight = info.get(1);
+        List<Double> info = GUI.getPassengerBagInfo();                      //get user bag info
+        if (info.get(0) < 0) {                                              //if -1 bag not wanting added
+            return -1.0;                                                    //pass back
+        } else {                                                            //bag wants added
+            double bagVol = info.get(0);                                    //volume is first entry
+            double bagWeight = info.get(1);                                 //weight is second
+            while ((bagVol > maxBagSize) || (bagWeight > maxBagWeight)) {   //while bag is over weight or over size
+                GUI.ErrorScreen(3);                              //show error screen (this line wont line up whats that about?)
+                info = GUI.getPassengerBagInfo();                           //re request bag data
+                bagVol = info.get(0);                                       //refresh
+                bagWeight = info.get(1);                                    //refresh
             }
-            double fee = 0.0;
+            double fee = 0.0;                                               //initalise fee counter
             //generate fees
-            fee += (bagNumber * feePerExtraBag);
-            if (bagVol > excessBagSize) {
-                fee += excessFee;
+            fee += (bagNumber * feePerExtraBag);                            //fee per bag > 1
+            if (bagVol > excessBagSize) {                                   //is bag excess volume
+                fee += excessFee;                                           //add fee
             }
-            if (bagWeight > excessBagWeight) {
-                fee += excessFee;
+            if (bagWeight > excessBagWeight) {                              // is bag excess weight
+                fee += excessFee;                                           //add fee
             }
             //request fee from user and add bag
-            if (fee > 0) {
-                if (GUI.requestFees(fee)) {
-                    Bag b = new Bag(bagVol, bagWeight);
-                    waitingRoom.get(reference).addBag(b);
-                    return fee;
+            if (fee > 0) {                                                  //if there is a fee
+                if (GUI.requestFees(fee)) {                                 //call gui to request payment
+                    Bag b = new Bag(bagVol, bagWeight);                     //create bag
+                    waitingRoom.get(reference).addBag(b);                   //add bag to passenger in waiting room
+                    return fee;                                             //return the totalled fee of bag
                 } else {
-                    GUI.ErrorScreen(4);
-                    return 0.0;
+                    GUI.ErrorScreen(4);                          //fee not paid show error screen (this one too??)
+                    return addBagToPassenger(reference, bagNumber);         //see if customer wants to add different bag instead
                 }
-            } else { //no fee required, just add bag
-                Bag b = new Bag(bagVol, bagWeight);
-                waitingRoom.get(reference).addBag(b);
-                return 0.0;
+            } else {                                                        //no fee required, just add bag
+                Bag b = new Bag(bagVol, bagWeight);                         //create bag
+                waitingRoom.get(reference).addBag(b);                       //add bag to customer in waiting room
+                return 0.0;                                                 //return no fee
             }
         }
     }
 
     /*Will call each flight to generate a report and store in list*/
-    private List<String> collectReports(){
+    private List<String> collectReports() {
         //done
-        Set<String> flightList = planes.keySet();
-        List<String> reports = new ArrayList<>();
+        Set<String> flightList = planes.keySet();   //generate iterable
+        List<String> reports = new ArrayList<>();   //initialise list
 
-        for(String key : flightList){
-            reports.add(planes.get(key).report());
+        for (String key : flightList) {             //iterate over map
+            reports.add(planes.get(key).report());  //request each flight to gen report and store in list
         }
 
-        return reports;
+        return reports;                             //return list
     }
 
     /*Takes in list of reports and formats them into longform final output string
-    * write this to file as well as return*/
-    private String writeReport(){
+     * write this to file as well as return*/
+    private String writeReport() {
         //done
-        String fullReport = "";
-        List<String> reports = collectReports();
+        String fullReport = "";                                                         //full report string
+        List<String> reports = collectReports();                                        //grab all flight reports
 
 
         fullReport += "Report from completed session\n";
         fullReport += java.time.LocalDate.now() + "\n\n";
-        for(String line : reports){
-            fullReport += line;
+        for (String line : reports) {
+            fullReport += line; //add each report
             fullReport += "\n";
         }
+        fullReport += "Total passengers not checked in: " + waitingRoom.size();         //final line
 
-        String fileName = "report" + java.time.LocalDate.now() + java.time.LocalTime.now() ;
-        try{
-            File write = new File(fileName);
-            if(!write.exists()) {
-                write.createNewFile();
+        String fileName = "report" + System.currentTimeMillis() + ".txt";               //make new file name
+        try {
+            File write = new File(fileName);                                            //file
+            if (!write.exists()) {                                                      //if its not already real
+                write.createNewFile();                                                  //make it
+                //System.out.println("File created successfully");
+            } else {
+                System.out.println("File already exits\nReports not saved");
             }
-        } catch(IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
         }
 
-        try{
-            FileWriter fw = new FileWriter(fileName);
-            fw.write(fullReport);
-        } catch(IOException e){
+        try {
+            BufferedWriter fw = new BufferedWriter(new FileWriter(fileName));           //writer
+            fw.write(fullReport);                                                       //write the whole report string
+            fw.close();
+            //System.out.println("File written successfully");
+        } catch (IOException e) {
             e.printStackTrace();
         }
-        return fullReport;
-    }
-
-    /*Main, duh*/
-    public static void main(String[] args){
-        Airport a = new Airport();
-        a.run();
+        return fullReport;                                                              //return the whole report string
     }
 }
--- src/CheckInGUI.java
+++ src/CheckInGUI.java
@@ -3,53 +3,54 @@ import java.util.List;
 
 public class CheckInGUI {
 
-    public CheckInGUI(){}
+    public CheckInGUI() {
+    }
 
     /*Will generate the standard gui layout
-    * Home screen with no required input from user
-    * Just a logo or something*/
-    public void createAndShowGUI(){
+     * Home screen with no required input from user
+     * Just a logo or something*/
+    public void createAndShowGUI() {
         //to do
     }
 
     /*Will give the user the option to check in passenger or Exit
-    * If check in passenger then request reference code and last name
-    * Pass these back (comma separated)
-    * If exit pass back "EXIT" to gen reports*/
-    public String getUserInput(){
+     * If check in passenger then request reference code and last name
+     * Pass these back (comma separated)
+     * If exit pass back "EXIT" to gen reports*/
+    public String getUserInput() {
         //to do
-        return "";
+        return "EXIT";
     }
 
     /*Will ask user if they want to add a bag
-    * if yes then ask user
-    * for bag dimensions and weight
-    * convert x,y,z dimensions to m^3 volume
-    * pass back volume, weight
-    * if no then pass back -1*/
-    public List<Double> getPassengerBagInfo(){
+     * if yes then ask user
+     * for bag dimensions and weight
+     * convert x,y,z dimensions to m^3 volume
+     * pass back volume, weight
+     * if no then pass back -1*/
+    public List<Double> getPassengerBagInfo() {
         //to do
         return new ArrayList<>();
     }
 
     /*Will request the user pay a fee for excess baggage
-    * return true to indicate some sort of payment procedure has been passed
-    * return false to indicate customer refusal to pay and bag should not be added*/
-    public Boolean requestFees(double fee){
+     * return true to indicate some sort of payment procedure has been passed
+     * return false to indicate customer refusal to pay and bag should not be added*/
+    public Boolean requestFees(double fee) {
         //to do
         return true;
     }
 
     /*Show an error screen based on error code
-    * 0: Generic error
-    * 1: Booking code could not be found
-    * 2: Booking code and last name did not match
-    * 3: Bag dimensions unacceptable
-    * 4: Baggage fees unpaid, bag not added
-    *
-    * Possibly more to come
-    * Will return "OK" or something to indicate user wants to proceed*/
-    public String ErrorScreen(int errorCode){
+     * 0: Generic error
+     * 1: Booking code could not be found
+     * 2: Booking code and last name did not match
+     * 3: Bag dimensions unacceptable
+     * 4: Baggage fees unpaid, bag not added
+     *
+     * Possibly more to come
+     * Will return "OK" or something to indicate user wants to proceed*/
+    public String ErrorScreen(int errorCode) {
         //to do
         return "";
     }
